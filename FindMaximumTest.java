3edcb98e4c9ea234a49600d0cfed1768be89806c
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-11 18:02:24 +0530

    Added: Ability to find maximum of three Floats

--- FindMaximumTest.java
+++ FindMaximumTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 public class FindMaximumTest {
 	
 	FindMaximum findMaximum = new FindMaximum();
-	
+	// UC 1
 	@Test
 	public void givenThreeIntValue_whenFirstIsMax_returnFirst() {
 		Integer first = 10;
 		Integer second = -3;
 		Integer third = 5;
-		assertEquals(first, findMaximum.findMax(first, second, third));
+		assertEquals(first, findMaximum.findMaxInt(first, second, third));
 	}
 	
 	@Test
@@ -20,7 +20,7 @@ public class FindMaximumTest {
 		Integer first = 10;
 		Integer second = 33;
 		Integer third = 5;
-		assertEquals(second, findMaximum.findMax(first, second, third));
+		assertEquals(second, findMaximum.findMaxInt(first, second, third));
 	}
 	
 	@Test
@@ -28,7 +28,33 @@ public class FindMaximumTest {
 		Integer first = 10;
 		Integer second = -3;
 		Integer third = 55;
-		assertEquals(third, findMaximum.findMax(first, second, third));
+		assertEquals(third, findMaximum.findMaxInt(first, second, third));
 	}
+	
+	//UC 2
+	@Test
+	public void givenThreeFloatValue_whenFirstIsMax_returnFirst() {
+		Float first = 10f;
+		Float second = -3f;
+		Float third = 5f;
+		assertEquals(first, findMaximum.findMaxFloat(first, second, third));
+	}
+	
+	@Test
+	public void givenThreeFloatValue_whenSecondIsMax_returnSecond() {
+		Float first = 10f;
+		Float second = 33f;
+		Float third = 5f;
+		assertEquals(second, findMaximum.findMaxFloat(first, second, third));
+	}
+	
+	@Test
+	public void givenThreeFloatValue_whenThirdIsMax_returnThird() {
+		Float first = 10f;
+		Float second = -3f;
+		Float third = 55f;
+		assertEquals(third, findMaximum.findMaxFloat(first, second, third));
+	}
+	
 
 }
